c187f2dd3bbe50cfbb55ded3e03bf2d3e931e7e4
Rithika <rganga@uci.edu>
2025-03-03 21:57:01 -0800

    Base of project.py

---
+++ project.py
@@ -0,0 +1,47 @@
+import mysql.connector
+import sys
+import csv
+import os
+
+
+def import_data(folder_name):
+    pass
+
+def insert_viewer():
+    pass
+
+def add_genre():
+    pass
+
+def delete_viewer():
+    pass
+
+def insert_movie():
+    pass
+
+def insert_session():
+    pass
+
+def update_release():
+    pass
+
+def get_releases_reviewed():
+    pass
+
+def get_popular_releases():
+    pass
+
+def release_title():
+    pass
+
+def get_active_viewers():
+    pass
+
+def videos_reviewed_count():
+    pass
+
+
+
+
+if name == "__main__":
+    pass
