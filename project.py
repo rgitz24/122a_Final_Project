c82fd8b2ee905cf01afcec2a508bfcf0e29ecf81
nntran15 <nathantran1506@gmail.com>
2025-03-13 19:11:59 -0700

    Insert_movie() first test

--- project.py
+++ project.py
@@ -353,7 +353,8 @@ def main():
     # Available functions
     functions = {
         'import': lambda: import_data(params[0]),
-        'insertViewer': lambda: insert_viewer(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], params[8], params[9], params[10], params[11])
+        'insertViewer': lambda: insert_viewer(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7], params[8], params[9], params[10], params[11]),
+        'insertMovie': lambda: insert_movie(params[0], params[1])
     }
 
     # Run functions
